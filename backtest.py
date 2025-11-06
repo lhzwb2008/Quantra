0cc34d87cdfee4d209f0fa057c11510390cd7d88
Wenbo Zhang <lhzwb2008@163.com>
2025-11-06 09:52:12 +0800

    up

--- backtest.py
+++ backtest.py
@@ -1599,8 +1599,8 @@ if __name__ == "__main__":
         'ticker': 'QQQ',
         'initial_capital': 13800,
         'lookback_days':1,
-        'start_date': date(2025, 1, 1),
-        'end_date': date(2025, 10, 25),
+        'start_date': date(2024, 7, 1),
+        'end_date': date(2025, 7, 2),
         'check_interval_minutes': 15 ,
         'enable_transaction_fees': True,  # 是否启用手续费计算，False表示不计算手续费
         'transaction_fee_per_share': 0.008166,
