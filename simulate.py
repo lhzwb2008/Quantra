e8e144ee2bf615a562b6b8f0a091e46d989800ab
Wenbo Zhang <lhzwb2008@163.com>
2025-12-14 10:40:39 +0800

    up

--- simulate.py
+++ simulate.py
@@ -20,7 +20,7 @@ TRADING_START_TIME = (9, 40)  # 交易开始时间：9点40分
 TRADING_END_TIME = (15, 40)   # 交易结束时间：15点40分
 MAX_POSITIONS_PER_DAY = 10
 LOOKBACK_DAYS = 1
-LEVERAGE = 2.3 # 杠杆倍数，默认为1倍
+LEVERAGE = 2.8 # 杠杆倍数，默认为1倍
 K1 = 1 # 上边界sigma乘数
 K2 = 1 # 下边界sigma乘数
 
